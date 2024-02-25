fe7cf66c73921d78af21f63ec28aa172bc1ea393
mizuno <mifukata-nozumi@outlook.jp>
2024-02-26 05:00:16 +0900

    内部表現の要素アクセス実装

--- include/tunum/fmpint.hpp
+++ include/tunum/fmpint.hpp
@@ -7,6 +7,7 @@
 #include <algorithm>
 #include <array>
 #include <ranges>
+#include <stdexcept>
 
 namespace tunum
 {
@@ -47,7 +48,7 @@ namespace tunum
             , lower(v < 0 ? -v : v)
         {
             if constexpr (size == sizeof(v))
-                this->upper = std::rotl(v < 0 ? -v : v, half_size);
+                this->upper = std::rotl(v < 0 ? -v : v, half_size * 8);
         }
 
         // 異なるサイズのfmpintから生成(内部表がんが等しいか小さいもの)
@@ -79,34 +80,24 @@ namespace tunum
         // 要素アクセス
         // -------------------------------------------
 
-    //     constexpr auto get_lower_bit_array() const noexcept
-    //     {
-    //         if constexpr (std::integral<half_fmpint>) return std::bitset{this->lower};
-    //         else return this->lower->to_bitarray_without_sign();
-    //     }
+        // 内部表現へのアクセス
+        constexpr base_data_t at(int i) const
+        {
+            constexpr auto data_size = sizeof(base_data_t);
+            constexpr auto data_length = size / data_size;
+            constexpr auto half_data_length = data_length / 2;
+            if (i >= data_length) throw std::out_of_range{""};
 
-    //     constexpr auto get_upper_bit_array() const noexcept
-    //     {
-    //         if constexpr (std::integral<half_fmpint>) return std::bitset{this->upper};
-    //         else return this->upper->to_bitarray_without_sign();
-    //     }
-
-    //     // 符号なしのビット列の表現をbooleanの配列として返却
-    //     constexpr auto to_bitarray_without_sign() const noexcept
-    //     {
-    //         constexpr auto data_length = size * 8;
-    //         constexpr auto upper_index_range = std::views::iota(0, data_length >> 1);
-    //         constexpr auto lower_index_range = std::views::iota(data_length >> 1, data_length);
-    //         const auto lower_bit_array = this->get_lower_bit_array();
-    //         const auto upper_bit_array = this->get_upper_bit_array();
-    //         auto buf = std::array<bool, data_length>{};
-    //         for (int i : std::views::iota(0, data_length >> 1))
-    //             buf[i] = lower_bitset[i];
-    //         for (int i : std::views::iota(data_length >> 1, data_length))
-    //             buf[i] = upper_bitset[i - data_length >> 1];
-    //         return buf;
-    //     }
-
+            const auto is_lower = half_data_length > i;
+            if constexpr (std::integral<half_fmpint>)
+                return is_lower ? this->lower : this->upper;
+            else {
+                const int next_i = i % half_data_length;
+                return is_lower
+                    ? this->lower.at(next_i)
+                    : this->upper.at(next_i);
+            }
+        }
 
     // private:
     //     static constexpr auto add(std::uint32_t v1, std::uint32_t v2) { return min_size_fmpint{std::uint64_t(v1) + std::uint64_t(v2)}; }
@@ -148,6 +139,7 @@ namespace tunum
 
     using int128_t = fmpint<(sizeof(std::uint64_t) << 1)>;
     using int256_t = fmpint<(sizeof(std::uint64_t) << 2)>;
+    using int512_t = fmpint<(sizeof(std::uint64_t) << 3)>;
 }
 
 #endif
--- test/fmpint_test.cpp
+++ test/fmpint_test.cpp
@@ -1,12 +1,13 @@
 #include <gtest/gtest.h>
 #include <tunum/fmpint.hpp>
 
+using int128_t_2 = tunum::fmpint<15>;
+using int64_t_1 = tunum::fmpint<0>;
+using int64_t_2 = tunum::fmpint<8>;
+constexpr auto uint32_max = ~std::uint32_t{};
+
 TEST(TunumFmpintTest, ConstructorTest)
 {
-    using int128_t_2 = tunum::fmpint<15>;
-    using int64_t_1 = tunum::fmpint<0>;
-    using int64_t_2 = tunum::fmpint<8>;
-    constexpr auto uint32_max = ~std::uint32_t{};
 
     ASSERT_EQ(tunum::int128_t::size, int128_t_2::size);
     ASSERT_EQ(int64_t_1::size, int64_t_2::size);
@@ -19,7 +20,7 @@ TEST(TunumFmpintTest, ConstructorTest)
     constexpr auto v3 = int128_t_2{std::int32_t(1234)};
     constexpr auto v4 = int64_t_2{std::int32_t(-5678)};
     constexpr auto v5 = int128_t_2{~std::uint64_t{}};
-    constexpr auto v6 = int64_t_2{~std::uint64_t{}};
+    constexpr auto v6 = int64_t_2{~std::uint64_t{} << 2};
 
     ASSERT_EQ(v3.lower.lower, 1234);
     ASSERT_EQ(v3.lower.upper, 0);
@@ -30,8 +31,8 @@ TEST(TunumFmpintTest, ConstructorTest)
     ASSERT_TRUE(v5.lower.lower == uint32_max);
     ASSERT_TRUE(v5.lower.upper == uint32_max);
     ASSERT_TRUE(v5.upper.lower == 0);
-    ASSERT_TRUE(v6.lower == uint32_max);
-    ASSERT_TRUE(v6.upper == uint32_max);
+    ASSERT_EQ(v6.lower, uint32_max << 2);
+    ASSERT_EQ(v6.upper, uint32_max);
 
     // fmpintによる初期化
     constexpr auto v7 = int128_t_2{v3};         // 同じ型
@@ -51,4 +52,21 @@ TEST(TunumFmpintTest, ConstructorTest)
     ASSERT_EQ(v10.lower, uint32_max);
     ASSERT_EQ(v10.upper, uint32_max);
     ASSERT_EQ(v10.sign, false);
+}
+
+TEST(TunumFmpintTest, ElementAccessTest)
+{
+    auto v1 = tunum::int128_t{(~std::uint64_t{}) << 2};
+    v1.upper = 5;
+
+    ASSERT_EQ(v1.at(0), uint32_max << 2);
+    ASSERT_EQ(v1.at(0), v1.lower.lower);
+    ASSERT_EQ(v1.at(1), uint32_max);
+    ASSERT_EQ(v1.at(1), v1.lower.upper);
+    ASSERT_EQ(v1.at(2), 5);
+    ASSERT_EQ(v1.at(2), v1.upper.lower);
+    ASSERT_EQ(v1.at(3), 0);
+    ASSERT_EQ(v1.at(3), v1.upper.upper);
+    ASSERT_THROW(v1.at(4), std::out_of_range);
+    ASSERT_THROW(v1.at(5), std::out_of_range);
 }
