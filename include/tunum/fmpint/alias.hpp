30148f6b469ef284fe2f9233a79dec439301a4cd
mizuno <mifukata-nozumi@outlook.jp>
2024-03-01 12:51:43 +0900

    fmpintをファイル分割

---
+++ include/tunum/fmpint/alias.hpp
@@ -0,0 +1,18 @@
+#ifndef TUNUM_INCLUDE_GUARD_TUNUM_FMPINT_ALIAS_HPP
+#define TUNUM_INCLUDE_GUARD_TUNUM_FMPINT_ALIAS_HPP
+
+#include TUNUM_COMMON_INCLUDE(fmpint/core.hpp)
+
+namespace tunum
+{
+
+    // -------------------------------------------
+    // エイリアス定義
+    // -------------------------------------------
+
+    using int128_t = fmpint<(sizeof(std::uint64_t) << 1)>;
+    using int256_t = fmpint<(sizeof(std::uint64_t) << 2)>;
+    using int512_t = fmpint<(sizeof(std::uint64_t) << 3)>;
+}
+
+#endif
---
+++ include/tunum/fmpint/operator.hpp
@@ -0,0 +1,50 @@
+#ifndef TUNUM_INCLUDE_GUARD_TUNUM_FMPINT_OPERATOR_HPP
+#define TUNUM_INCLUDE_GUARD_TUNUM_FMPINT_OPERATOR_HPP
+
+#include TUNUM_COMMON_INCLUDE(fmpint/core.hpp)
+
+namespace tunum
+{
+    // -------------------------------------------
+    // 演算子オーバーロード(グローバル)
+    // -------------------------------------------
+
+#ifndef TUNUM_FUNC_MAKE_FMPINT_OPERATOR
+#define TUNUM_FUNC_MAKE_FMPINT_OPERATOR(op_name, op) template <TuFmpIntegral T1, TuIntegral T2> \
+    constexpr auto op_name(const T1& l, const T2& r) { return get_large_integral_t<T1, T2>{l} op r; } \
+    template <TuFmpIntegral T> \
+    constexpr auto op_name(std::integral auto l, const T&  r) { return T{l} op r; }
+#endif
+
+    template <TuFmpIntegral T>
+    constexpr auto operator<<(const T& l, std::size_t r) { return T{l} <<= r; }
+
+    template <TuFmpIntegral T>
+    constexpr auto operator>>(const T& l, std::size_t r) { return T{l} >>= r; }
+
+    TUNUM_FUNC_MAKE_FMPINT_OPERATOR(operator|, |=)
+    TUNUM_FUNC_MAKE_FMPINT_OPERATOR(operator&, &=)
+    TUNUM_FUNC_MAKE_FMPINT_OPERATOR(operator^, ^=)
+
+    template <TuFmpIntegral T1, TuIntegral T2>
+    constexpr auto operator<=>(const T1& l, const T2& r)
+    {
+        using large_integral_t = get_large_integral_t<T1, T2>;
+        return large_integral_t{l}._compare(large_integral_t{r});
+    }
+    template <TuFmpIntegral T>
+    constexpr auto operator<=>(std::integral auto l, const T& r) { return T{l}._compare(r); }
+
+    constexpr bool operator==(const TuFmpIntegral auto& l, const TuIntegral auto& r) { return (l <=> r) == 0; }
+    constexpr bool operator==(std::integral auto l, const TuFmpIntegral auto& r) { return (l <=> r) == 0; }
+
+    TUNUM_FUNC_MAKE_FMPINT_OPERATOR(operator+, +=)
+    TUNUM_FUNC_MAKE_FMPINT_OPERATOR(operator-, -=)
+    TUNUM_FUNC_MAKE_FMPINT_OPERATOR(operator*, *=)
+    TUNUM_FUNC_MAKE_FMPINT_OPERATOR(operator/, /=)
+    TUNUM_FUNC_MAKE_FMPINT_OPERATOR(operator%, %=)
+
+#undef TUNUM_FUNC_MAKE_FMPINT_OPERATOR
+}
+
+#endif
