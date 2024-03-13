6d3096061ec830e9dfb889d2eb31c57c8b8f79b3
mizuno <mifukata-nozumi@outlook.jp>
2024-03-14 02:25:45 +0900

    文字列の検証の動作テスト

--- test/fmpint_test.cpp
+++ test/fmpint_test.cpp
@@ -410,4 +410,11 @@ TEST(TunumFmpintTest, StringConstructorTest)
     constexpr auto fmp_int_v8 = 00000377'77777777'77777777'77777777'77777777'77777777_ufmp;
     EXPECT_EQ(fmp_int_v3, fmp_int_v7);
     EXPECT_EQ(fmp_int_v7, fmp_int_v8);
+
+    EXPECT_THROW(tunum::uint128_t{"0a123"}, std::invalid_argument);
+    EXPECT_THROW(tunum::uint128_t{"0b1201"}, std::invalid_argument);
+    EXPECT_THROW(tunum::uint128_t{"'12345"}, std::invalid_argument);
+    EXPECT_THROW(tunum::uint128_t{"12345A"}, std::invalid_argument);
+    EXPECT_THROW(tunum::uint128_t{"01238"}, std::invalid_argument);
+    EXPECT_THROW(tunum::uint128_t{"0X12''345"}, std::invalid_argument);
 }
