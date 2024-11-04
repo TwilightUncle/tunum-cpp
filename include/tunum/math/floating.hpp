c56b83685c3aab881f533daf30210830824d13cf
mizuno <mifukata-nozumi@outlook.jp>
2024-11-04 17:27:46 +0900

    数値分類関数実装

--- include/tunum/math/floating.hpp
+++ include/tunum/math/floating.hpp
@@ -6,16 +6,83 @@
 
 namespace tunum::_math_impl
 {
+    template <std::floating_point T>
+    inline constexpr T fma(T x, T y, T z) noexcept
+    {
+        if (!std::is_constant_evaluated())
+            return std::fma(x, y, z);
+        return x * y + z;
+    }
+
+    inline constexpr int fpclassify(std::floating_point auto x) noexcept
+    {
+        if (!std::is_constant_evaluated())
+            return std::fpclassify(x);
+        return floating_std_info{x}.get_fpclass();
+    }
+
+    inline constexpr int fpclassify(std::integral auto x) noexcept
+    { return fpclassify(double(x)); }
+
+    inline constexpr bool isfinite(std::floating_point auto x) noexcept
+    {
+        if (!std::is_constant_evaluated())
+            return std::isfinite(x);
+        return floating_std_info{x}.is_finity();
+    }
+
+    inline constexpr bool isfinite(std::integral auto x) noexcept
+    { return isfinite(double(x)); }
+
+    inline constexpr bool isinf(std::floating_point auto x) noexcept
+    {
+        if (!std::is_constant_evaluated())
+            return std::isinf(x);
+        return floating_std_info{x}.is_infinity();
+    }
+
+    inline constexpr bool isinf(std::integral auto x) noexcept
+    { return isinf(double(x)); }
+
+    inline constexpr bool isnan(std::floating_point auto x) noexcept
+    {
+        if (!std::is_constant_evaluated())
+            return std::isnan(x);
+        return floating_std_info{x}.is_nan();
+    }
+    
+    inline constexpr bool isnan(std::integral auto x) noexcept
+    { return isnan(double(x)); }
+
+    inline constexpr bool isnormal(std::floating_point auto x) noexcept
+    {
+        if (!std::is_constant_evaluated())
+            return std::isnan(x);
+        return floating_std_info{x}.is_normalized();
+    }
+
+    inline constexpr bool isnormal(std::integral auto x) noexcept
+    { return isnormal(double(x)); }
+
+    inline constexpr bool signbit(std::floating_point auto x) noexcept
+    {
+        if (!std::is_constant_evaluated())
+            return std::signbit(x);
+        return floating_std_info{x}.sign() < 0;
+    }
+
+    inline constexpr bool signbit(std::integral auto x) noexcept
+    { return signbit(double(x)); }
+    
     template <class T>
     requires (std::is_arithmetic_v<T>)
     inline constexpr T copysign(T x, T y) noexcept
     {
         if (!std::is_constant_evaluated())
             return std::copysign(x, y);
-        else
-            return (x < 0) == (y < 0) 
-                ? x
-                : x * -1;
+        return (x < 0) == (y < 0) 
+            ? x
+            : x * -1;
     }
 
     template <std::floating_point T>
@@ -43,6 +110,49 @@ namespace tunum::_math_impl
     // cpo
     // -------------------------------------------
 
+    struct fma_cpo
+    {
+        template <class T>
+        constexpr T operator()(T x, T y, T z) const
+        { return fma(x, y, z); }
+    };
+
+    struct fpclassify_cpo
+    {
+        constexpr int operator()(auto x) const
+        { return fpclassify(x); }
+    };
+
+    struct isfinite_cpo
+    {
+        constexpr bool operator()(auto x) const
+        { return isfinite(x); }
+    };
+
+    struct isinf_cpo
+    {
+        constexpr bool operator()(auto x) const
+        { return isinf(x); }
+    };
+
+    struct isnan_cpo
+    {
+        constexpr bool operator()(auto x) const
+        { return isnan(x); }
+    };
+
+    struct isnormal_cpo
+    {
+        constexpr bool operator()(auto x) const
+        { return isnormal(x); }
+    };
+
+    struct signbit_cpo
+    {
+        constexpr bool operator()(auto x) const
+        { return signbit(x); }
+    };
+
     struct copysign_cpo
     {
         template <class T>
@@ -67,6 +177,40 @@ namespace tunum::_math_impl
 
 namespace tunum
 {
+    // 乗算と加算の合成
+    // 実行時かつfma命令が有効であれば、fma命令を実行
+    // 上記以外では x * y + z
+    // @param x
+    // @param y
+    // @param z
+    // @return x * y + z
+    inline constexpr _math_impl::fma_cpo fma{};
+
+    // 数値分類を取得
+    // @param x 検査対象
+    // @return FP_INFINITE, FP_NAN, FP_NORMAL, FP_SUBNORMAL, FP_ZEROのいずれか、あるいは処理系独自定義の数値分類
+    inline constexpr _math_impl::fpclassify_cpo fpclassify{};
+
+    // 有限か判定
+    // @param x 検査対象
+    inline constexpr _math_impl::isfinite_cpo isfinite{};
+
+    // 無限大か判定
+    // @param x 検査対象
+    inline constexpr _math_impl::isinf_cpo isinf{};
+
+    // NaNであるか判定
+    // @param x 検査対象
+    inline constexpr _math_impl::isnan_cpo isnan{};
+
+    // 正規化数であるか判定
+    // @param x 検査対象
+    inline constexpr _math_impl::isnormal_cpo isnormal{};
+
+    // 負数であるか判定
+    // @param x 検査対象
+    inline constexpr _math_impl::signbit_cpo signbit{};
+
     // 符号のコピー
     // @param x 値担当
     // @param y 符号担当
--- test/math_test.cpp
+++ test/math_test.cpp
@@ -1,6 +1,110 @@
 #include <gtest/gtest.h>
 #include <tunum/math.hpp>
 
+using floating_limit_t = std::numeric_limits<float>;
+
+TEST(TunumMathTest, FpClassTest)
+{
+    constexpr auto nan = floating_limit_t::quiet_NaN();
+    constexpr auto inf = floating_limit_t::infinity();
+    constexpr auto zero = 0.f;
+    constexpr auto max_norm = floating_limit_t::max();
+    constexpr auto min_norm = floating_limit_t::min();
+    constexpr auto denorm = floating_limit_t::denorm_min();
+
+    constexpr auto fpclass_01 = tunum::fpclassify(nan);
+    constexpr auto fpclass_02 = tunum::fpclassify(inf);
+    constexpr auto fpclass_03 = tunum::fpclassify(-inf);
+    constexpr auto fpclass_04 = tunum::fpclassify(zero);
+    constexpr auto fpclass_05 = tunum::fpclassify(-zero);
+    constexpr auto fpclass_06 = tunum::fpclassify(max_norm);
+    constexpr auto fpclass_07 = tunum::fpclassify(-max_norm);
+    constexpr auto fpclass_08 = tunum::fpclassify(min_norm);
+    constexpr auto fpclass_09 = tunum::fpclassify(-min_norm);
+    constexpr auto fpclass_10 = tunum::fpclassify(denorm);
+    constexpr auto fpclass_11 = tunum::fpclassify(-denorm);
+    EXPECT_EQ(fpclass_01, std::fpclassify(nan));
+    EXPECT_EQ(fpclass_02, std::fpclassify(inf));
+    EXPECT_EQ(fpclass_03, std::fpclassify(-inf));
+    EXPECT_EQ(fpclass_04, std::fpclassify(zero));
+    EXPECT_EQ(fpclass_05, std::fpclassify(-zero));
+    EXPECT_EQ(fpclass_06, std::fpclassify(max_norm));
+    EXPECT_EQ(fpclass_07, std::fpclassify(-max_norm));
+    EXPECT_EQ(fpclass_08, std::fpclassify(min_norm));
+    EXPECT_EQ(fpclass_09, std::fpclassify(-min_norm));
+    EXPECT_EQ(fpclass_10, std::fpclassify(denorm));
+    EXPECT_EQ(fpclass_11, std::fpclassify(-denorm));
+
+    constexpr auto isfinite_1 = tunum::isfinite(nan);
+    constexpr auto isfinite_2 = tunum::isfinite(inf);
+    constexpr auto isfinite_3 = tunum::isfinite(zero);
+    constexpr auto isfinite_4 = tunum::isfinite(max_norm);
+    constexpr auto isfinite_5 = tunum::isfinite(min_norm);
+    constexpr auto isfinite_6 = tunum::isfinite(denorm);
+    EXPECT_EQ(isfinite_1, std::isfinite(nan));
+    EXPECT_EQ(isfinite_2, std::isfinite(inf));
+    EXPECT_EQ(isfinite_3, std::isfinite(zero));
+    EXPECT_EQ(isfinite_4, std::isfinite(max_norm));
+    EXPECT_EQ(isfinite_5, std::isfinite(min_norm));
+    EXPECT_EQ(isfinite_6, std::isfinite(denorm));
+
+    constexpr auto isinf_1 = tunum::isinf(nan);
+    constexpr auto isinf_2 = tunum::isinf(inf);
+    constexpr auto isinf_3 = tunum::isinf(zero);
+    constexpr auto isinf_4 = tunum::isinf(max_norm);
+    constexpr auto isinf_5 = tunum::isinf(min_norm);
+    constexpr auto isinf_6 = tunum::isinf(denorm);
+    EXPECT_EQ(isinf_1, std::isinf(nan));
+    EXPECT_EQ(isinf_2, std::isinf(inf));
+    EXPECT_EQ(isinf_3, std::isinf(zero));
+    EXPECT_EQ(isinf_4, std::isinf(max_norm));
+    EXPECT_EQ(isinf_5, std::isinf(min_norm));
+    EXPECT_EQ(isinf_6, std::isinf(denorm));
+
+    constexpr auto isnan_1 = tunum::isnan(nan);
+    constexpr auto isnan_2 = tunum::isnan(inf);
+    constexpr auto isnan_3 = tunum::isnan(zero);
+    constexpr auto isnan_4 = tunum::isnan(max_norm);
+    constexpr auto isnan_5 = tunum::isnan(min_norm);
+    constexpr auto isnan_6 = tunum::isnan(denorm);
+    EXPECT_EQ(isnan_1, std::isnan(nan));
+    EXPECT_EQ(isnan_2, std::isnan(inf));
+    EXPECT_EQ(isnan_3, std::isnan(zero));
+    EXPECT_EQ(isnan_4, std::isnan(max_norm));
+    EXPECT_EQ(isnan_5, std::isnan(min_norm));
+    EXPECT_EQ(isnan_6, std::isnan(denorm));
+
+    constexpr auto isnormal_1 = tunum::isnormal(nan);
+    constexpr auto isnormal_2 = tunum::isnormal(inf);
+    constexpr auto isnormal_3 = tunum::isnormal(zero);
+    constexpr auto isnormal_4 = tunum::isnormal(max_norm);
+    constexpr auto isnormal_5 = tunum::isnormal(min_norm);
+    constexpr auto isnormal_6 = tunum::isnormal(denorm);
+    EXPECT_EQ(isnormal_1, std::isnormal(nan));
+    EXPECT_EQ(isnormal_2, std::isnormal(inf));
+    EXPECT_EQ(isnormal_3, std::isnormal(zero));
+    EXPECT_EQ(isnormal_4, std::isnormal(max_norm));
+    EXPECT_EQ(isnormal_5, std::isnormal(min_norm));
+    EXPECT_EQ(isnormal_6, std::isnormal(denorm));
+
+    constexpr auto signbit_1 = tunum::signbit(nan);
+    constexpr auto signbit_2 = tunum::signbit(inf);
+    constexpr auto signbit_3 = tunum::signbit(zero);
+    constexpr auto signbit_4 = tunum::signbit(max_norm);
+    constexpr auto signbit_5 = tunum::signbit(min_norm);
+    constexpr auto signbit_6 = tunum::signbit(denorm);
+    constexpr auto signbit_7 = tunum::signbit(-nan);
+    constexpr auto signbit_8 = tunum::signbit(-zero);
+    EXPECT_EQ(signbit_1, std::signbit(nan));
+    EXPECT_EQ(signbit_2, std::signbit(inf));
+    EXPECT_EQ(signbit_3, std::signbit(zero));
+    EXPECT_EQ(signbit_4, std::signbit(max_norm));
+    EXPECT_EQ(signbit_5, std::signbit(min_norm));
+    EXPECT_EQ(signbit_6, std::signbit(denorm));
+    EXPECT_EQ(signbit_7, std::signbit(-nan));
+    EXPECT_EQ(signbit_8, std::signbit(-zero));
+ }
+
 TEST(TunumMathTest, CopySignTest)
 {
     constexpr auto copysign_1 = tunum::copysign(1, -2);
